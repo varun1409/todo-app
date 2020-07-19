eff14e2bbaeaf582f0730c3c203c445712684e0b
Varun <varun.chandra14@gmail.com>
2020-07-19 14:51:10 -0400

    implement input/output approach for todo

--- src/app/components/add-todo/add-todo.component.ts
+++ src/app/components/add-todo/add-todo.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-add-todo',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTodoComponent implements OnInit {
 
+  @Output() itemCreated = new EventEmitter();
+
+  item:List = {
+    id: '',
+    title: '',
+    description: ''
+  };
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addItemToList(){
+    const item: List = {
+      id: '0',
+      title: this.item.title,
+      description: this.item.description
+    }
+    this.itemCreated.emit(item);
+  }
 }
